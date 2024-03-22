529f081f923aae5cf504738e19dfd4fd6c76d57e
unknown <okolosarah402@gmail.com>
2024-03-22 15:57:22 +0100

    updated readme file

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![Desktop preview screenshot](./images/intro-section-dropdown-menu-desktop-screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/Sarah-okolo/intro-section-with-dropdown-navigation-main)
+- Live Site URL: [Add live site URL here](https://introsection-with-dropdown-navigation.netlify.app/)
+
+## My process
+
+I created the mobile view first then moved on to create the tablet view, and then the desktop view.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+I learned how to create a beautiful custom dropdown menu when building this project
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/sarah-okolo)
--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-	const burgerMenu = document.getElementById("burgerIcon");
-  				const modal = document.getElementById("modal");
-  				const closeMenu = document.getElementById("closeMenuIcon");
-  				const featuresDropdown = document.getElementById("featuresDropdown");
-  				const companyDropdown = document.getElementById("companyDropdown");
-  				const featuresSubMenu = document.querySelector(".featuresSubMenu");
-  				const companySubMenu = document.querySelector(".companySubMenu");
-  				const arrow1 = document.getElementById("arrow1");
-  				const arrow2 = document.getElementById("arrow2");
-  				const heroImage = document.getElementById("heroImage");
-  				
-  				//opens the menu modal onclick.
-  				burgerMenu.onclick=()=>{
-  				   modal.style.width="100vw";
-  				}
-  				
-  				//closes the menu modal onclick.
-  				closeMenu.onclick=()=>{
-  				   modal.style.width="0";
-  				  }
-  				  
-  				//shows the featuresList submenu. onclick;
-  				featuresDropdown.onclick=()=>{
-  	featuresSubMenu.classList.toggle("block");
-  	arrow1.classList.toggle("rotate");
-  				}
-  				
-  				//shows the companyList submenu.
-  				companyDropdown.onclick=()=>{
-  	companySubMenu.classList.toggle("block");
-  	arrow2.classList.toggle("rotate");
-  				}
+const burgerMenu = document.getElementById("burgerIcon");
+const modal = document.getElementById("modal");
+const closeMenu = document.getElementById("closeMenuIcon");
+const featuresDropdown = document.getElementById("featuresDropdown");
+const companyDropdown = document.getElementById("companyDropdown");
+const featuresSubMenu = document.querySelector(".featuresSubMenu");
+const companySubMenu = document.querySelector(".companySubMenu");
+const arrow1 = document.getElementById("arrow1");
+const arrow2 = document.getElementById("arrow2");
+const heroImage = document.getElementById("heroImage");
 
+//opens the menu modal onclick.
+burgerMenu.onclick = () => {
+  modal.style.width = "100vw";
+};
 
-     //checks for the change in viewport, and if it is greater than 500px, then it changes the src value of the hero image to the desktop design, and uf it is less, it changes it to the mobile design.
-     if(window.innerWidth > 850){
-     				heroImage.src="images/image-hero-desktop.png";
-     }
-     else{
-      				heroImage.src="images/image-hero-mobile.png";
-      }
+//closes the menu modal onclick.
+closeMenu.onclick = () => {
+  modal.style.width = "0";
+};
+
+//shows the featuresList submenu. onclick;
+featuresDropdown.onclick = () => {
+  featuresSubMenu.classList.toggle("block");
+  arrow1.classList.toggle("rotate");
+};
+
+//shows the companyList submenu.
+companyDropdown.onclick = () => {
+  companySubMenu.classList.toggle("block");
+  arrow2.classList.toggle("rotate");
+};
+
+//checks for the change in viewport, and if it is greater than 500px, then it changes the src value of the hero image to the desktop design, and uf it is less, it changes it to the mobile design.
+if (window.innerWidth > 850) {
+  heroImage.src = "images/image-hero-desktop.png";
+} else {
+  heroImage.src = "images/image-hero-mobile.png";
+}
